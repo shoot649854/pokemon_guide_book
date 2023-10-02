63b174226b7e85908ae1c444a4b42c78cb82184b
shoot649854 <jojoto8845@icloud.com>
2023-10-02 13:11:13 -0700

    Pokemon Guide book with 150 pokemons in one page

--- pokemon_app/src/App.tsx
+++ pokemon_app/src/App.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import PokemonComponent from './pokemon_app';
+import {PokeCard} from './pokemon_app';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <PokemonComponent />
+        <PokeCard />
       </header>
     </div>
   );
