b5c59ee22d3ab77d3321a2a06c863eedd5622b5f
morisaki-s-ww <morisaki.s@world-wing.com>
2023-10-08 16:17:41 -0700

    ⚒ Code Refactoring

--- pokemon_app/src/page/PokeApp.tsx
+++ pokemon_app/src/page/PokeApp.tsx
@@ -1,14 +1,8 @@
 import { useEffect, useState } from 'react';
 import * as React from 'react';
-
 import { Link } from 'react-router-dom';
 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import { Card, CardContent, CardMedia, Typography} from '@mui/material';
 
 import { Pokemon } from '../type/type';
 
