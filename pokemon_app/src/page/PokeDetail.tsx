36e8d60326449c10d1a7988429c98ee0d629145b
shoot649854 <jojoto9945@icloud.com>
2024-01-06 15:56:46 -0800

    📦Adding Dependency

--- pokemon_app/src/page/PokeDetail.tsx
+++ pokemon_app/src/page/PokeDetail.tsx
@@ -9,41 +9,33 @@ import { Pokemon } from '../type/type';
 export const PokeDetail = () => {
     const { id } = useParams();
     const PokemonLink = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+    const [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
     useEffect(() => {
-        fetchPokemon().then((pokemonArray) => {
-            setPokemon(pokemonArray);
+        // Modify the fetchPokemon function to fetch a single Pokemon
+        fetchPokemon(id).then((pokemonData) => {
+            setPokemon(pokemonData);
         });
     }, [id]);
+
     return (
         <Grid container spacing={2}>
-        <Grid item xs={12}>
-            <Typography variant="h4"> {pokemon.name} </Typography>
-            <Typography variant="subtitle1">No.150</Typography>
-        </Grid>
-        <Grid item xs={6}>
-            <img src={pokemon.image} />
-        </Grid>
-        <Grid item xs={6}>
-            <table>
-            <tbody>
-                <tr>
-                <td>Height:</td>
-                <td>2.0 m</td>
-                </tr>
-                <tr>
-                <td>Weight:</td>
-                <td>122.0 kg</td>
-                </tr>
-                <tr>
-                <td>HP:</td>
-                <td>106</td>
-                </tr>
-            </tbody>
-            </table>
-            <img src="https://via.placeholder.com/50x50" alt="Legendary" />
-        </Grid>
+            <Grid item xs={12}>
+                {/* Check if pokemon is not null before accessing its properties */}
+                {pokemon && (
+                    <>
+                        <Typography variant="h4"> {pokemon.name} </Typography>
+                        <Typography variant="subtitle1">No. {pokemon.id}</Typography>
+                    </>
+                )}
+            </Grid>
+            <Grid item xs={6}>
+                {/* Check if pokemon is not null before accessing its properties */}
+                {pokemon && <img src={pokemon.image} alt={pokemon.name} />}
+            </Grid>
+            <Grid item xs={6}>
+                <img src="https://via.placeholder.com/50x50" alt="Legendary" />
+            </Grid>
         </Grid>
     );
-}
+};
