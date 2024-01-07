4f5cad8e58a7b1bfa17763cbef6398b93614e467
shoot649854 <jojoto9945@icloud.com>
2024-01-07 11:43:57 -0800

    👍Organizing Router and Naviagete

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import {PokeCard} from './page/PokeApp';
-import {PokeDetail} from './page/PokeDetail';
+import { PokeCard } from './page/PokeApp';
+import { PokeDetail } from './page/PokeDetail';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Router>
+        <Router>
           <Routes>
             <Route path="/" element={<PokeCard />} />
+            {/* <Route path="/pokemon_guide_book" element={<PokeCard />} /> */}
             <Route path="/pokemon/:id" element={<PokeDetail />} />
+            <Route path="/pokemon_guide_book" element={<Navigate to="/" />} />
           </Routes>
         </Router>
       </header>
